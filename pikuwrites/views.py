0a1a5416adcc51f02b31fffeee3567770867d1fb
himanshu <himanhu.behl@gmail.com>
2021-04-28 19:31:03 +0530

    only like button left

--- pikuwrites/views.py
+++ pikuwrites/views.py
@@ -1,17 +1,72 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post,IpModel
 from pip._vendor.requests.api import request
 from django.views.generic import ListView,DetailView,CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 #def index(request):
 #   return HttpResponse('Hello')
 
-class IndexView(ListView):
-    model = Post
-    template_name = 'index.html'
+def index(request):
+    poems = Post.objects.all().filter(type="poem")
+    quotes = Post.objects.all().filter(type="quote")
+    mydict = {
+        "poems" : poems,
+        "quotes" : quotes,
+        "object_list" : Post.objects.all()
+    }
+    return render(request,'index.html',context=mydict)
+
+def terms(request):
+    return render(request,'terms-and-conditions.html')
+
+def privacyPolicy(request):
+    return render(request,'privacy.html')
+
+# class IndexView(ListView):
+#     model = Post
+#     template_name = 'index.html'
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWADED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
     
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_details.html'
+    context_object_name = 'post'
+    template_name = 'poem.html'
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        
+        like_status = False
+        ip = get_client_ip(request)
+        if self.object.likes.filter(id=IpModel.objects.get(ip=ip).id).exists():
+            like_status = True
+        else:
+            like_status = False
+        
+        context['like_status'] = like_status
+        
+        return self.render_to_response(context)
 
+def poemlike(request,pk):
+    post_id = request.POST.get('poem-id')
+    post = Post.objects.get(pk=post_id)
+    ip = get_client_ip(request)
+    
+    if not IpModel.objects.filter(ip=ip).exists():
+        IpModel.objects.create(ip=ip)
+    
+    if post.likes.filter(id=IpModel.objects.get(ip=ip).id).exists():
+        post.likes.remove(IpModel.objects.get(ip=ip))
+    else :
+        post.likes.add(IpModel.objects.get(ip=ip))
+    
+    return HttpResponseRedirect(reverse('post-detail' , args=[post_id]))
