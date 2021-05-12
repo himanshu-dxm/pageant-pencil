470b4f22958c93524acffed265850fd494f29a16
himanshu <himanhu.behl@gmail.com>
2021-05-12 20:17:59 +0530

    added -1 to get ip

--- pikuwrites/views.py
+++ pikuwrites/views.py
@@ -31,7 +31,7 @@ def privacyPolicy(request):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWADED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[x_forwarded_for.length-1]
+        ip = x_forwarded_for.split(',')[-1]
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
